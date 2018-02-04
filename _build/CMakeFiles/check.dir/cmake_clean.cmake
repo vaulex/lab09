b35db2969fca8e0093ce23ced0938621c673173a
vaulex <krasauchek.s.iy@ya.ru>
2018-02-04 23:14:47 +0300

    Lab06 finals..

---
+++ _build/CMakeFiles/check.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/check.dir/tests/test1.cpp.o"
+  "CMakeFiles/check.dir/tests/main.cpp.o"
+  "check.pdb"
+  "check"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/check.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ _build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/vaulex/vaulex/workspace/projects/lab06
+# Build directory: /home/vaulex/vaulex/workspace/projects/lab06/_build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(check "/home/vaulex/vaulex/workspace/projects/lab06/_build/check" "-s" "-r" "compact" "--use-colour" "yes")
