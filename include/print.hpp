c50de3a5762c9d4bb5142fb6f5ef0d5260a65fa4
vaulex <krasauchek.s.iy@ya.ru>
2018-02-05 11:43:42 +0300

    added doxygen.conf

--- include/print.hpp
+++ include/print.hpp
@@ -2,5 +2,5 @@
 #include <fstream>
 #include <iostream>
 
-void print(const std::string& text, std::ostream& out = std::cout);
-void print(const std::string& text, std::ofstream& out);
+void print(const std::string& text, std::ostream& out = std::cout);	//конструктор1
+void print(const std::string& text, std::ofstream& out);	//конструктор2
